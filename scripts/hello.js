45c4928f169d9b06376e478a427d9636e82e92bd
dickyyahya <dickyyahya25@gmail.com>
2026-03-24 12:30:06 +0700

    first commit

---
+++ scripts/hello.js
@@ -0,0 +1,16 @@
+//tipe data string
+document.writeln("Hello js");
+document.writeln("<br>");
+//nomor genap
+document.writeln(100);
+document.writeln("<br>");
+//nomor ganjil
+document.writeln(10.5);
+document.writeln("<br>");
+//tipe data boolen
+document.writeln(true);
+document.writeln("<br>");
+document.writeln(false);
+document.writeln("<br>");
+// nomor binari
+document.writeln(0b1111);
