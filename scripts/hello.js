0d58d9393fb238d36a95fca35afb9aacfdbdb40b
dickyyahya <dickyyahya25@gmail.com>
2026-03-24 12:31:23 +0700

    belajar escape sequence

--- scripts/hello.js
+++ scripts/hello.js
@@ -14,3 +14,15 @@ document.writeln(false);
 document.writeln("<br>");
 // nomor binari
 document.writeln(0b1111);
+
+//menambah string
+document.writeln("<br>");
+document.writeln("Nama : " + "yahya");
+
+// escape sequence
+document.writeln("<textarea cols='100' rows='10'>");
+document.writeln("Budi \nAgus \nSumanto");
+document.writeln("\\Home\\yahya");
+document.writeln('\"Progammer Handal\"');
+document.writeln("\'Sinau JS\'");
+document.writeln("</textarea>");
